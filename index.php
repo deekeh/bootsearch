d51a41f490e0b229e9f621c7dc45a7453ea63f86
Darshan Khot <43519784+deekeh@users.noreply.github.com>
2019-04-19 14:46:02 +0530

    Index file

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home | BootSearch</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/master.css">
+    <script type="text/javascript" src="js/options.js">
+
+    </script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <nav class="navbar navbar-light bg-light row mynav">
+        <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 row">
+          <a class="navbar-brand" href=""><img src="img/bs_logo.png" height="35rem" alt=""><span> | BootSearch</span></a>
+        </div>
+        <div class="col-lg-10 col-md-6 col-sm-12 col-xs-12">
+          <form class="form-inline row">
+            <input class="form-control mr-sm-2 col-lg-9 col-md-9 col-sm-9 col-xs-12" type="search" placeholder="Search" aria-label="Search">
+            <input class="btn btn-outline-success my-2 my-sm-0 col-lg-2 col-md-2 col-sm-2 col-xs-12" type="button" value="Search" onclick="sort();">
+          </form>
+        </div>
+      </nav><br>
+
+      <div class="row">
+        <div class="col-md-12 col-sm-12">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title" style="color: #00a500;">Bootstrap feature name</h5>
+              <p class="card-text">This is a bootstrap feature text. This text describes about all the features of the bootstrap class.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="content">
+
+      </div>
+
+    </div>
+  </body>
+</html>
