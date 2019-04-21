363e897cae2804efb90a490a4458220c12339910
Darshan Khot <43519784+deekeh@users.noreply.github.com>
2019-04-21 22:11:42 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,37 +3,30 @@
   <head>
     <meta charset="utf-8">
     <title>Home | BootSearch</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-    
+
     <link rel="stylesheet" href="css/master.css">
     <script type="text/javascript" src="js/options.js"></script>
   </head>
   <body>
     <div class="container-fluid">
       <nav class="navbar navbar-light bg-light row mynav">
-        <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 row">
+        <div class="">
           <a class="navbar-brand" href=""><img src="img/bs_logo.png" height="35rem" alt=""><span> | BootSearch</span></a>
         </div>
-        <div class="col-lg-10 col-md-6 col-sm-12 col-xs-12">
-          <form class="form-inline row">
-            <input class="form-control mr-sm-2 col-lg-9 col-md-9 col-sm-9 col-xs-12" type="search" placeholder="Search" aria-label="Search">
-            <input id='search_b' class="btn btn-outline-success my-2 my-sm-0 col-lg-2 col-md-2 col-sm-2 col-xs-12" type="button" value="Search" onclick="sort();">
+        <div class="">
+          <form class="form-inline" method="get">
+            <input class="form-control mr-sm-2 col-lg-8 col-md-8 col-sm-12 col-xs-12" id='search_term' type="search" placeholder="Search" aria-label="Search">
+            <input id='search_b' class="btn btn-outline-success my-2 my-sm-0 col-lg-3 col-md-3 col-sm-12 col-xs-12" type="button" value="Search" onclick="display_options();">
           </form>
         </div>
       </nav><br>
 
       <div id="content" class="list-group"></div>
-      <div id="description" style="display: none;">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
+      <div id="description" style="display: none;"></div>
     </div>
     <br>
   </body>
