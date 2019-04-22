578b050f597df489d8ee1df4afce12a9369576c8
Darshan Khot <43519784+deekeh@users.noreply.github.com>
2019-04-22 14:39:48 +0530

    Add files via upload

--- js/options.js
+++ js/options.js
@@ -8,24 +8,138 @@ var getdescription = function (search_term) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'data/bs_data.json', true);
   xhr.onload = function() {
-    // if (this.status == 200) {
+  // function xhr.onload() {
+    if (this.status == 200) {
+      console.log(this.responseText);
       var json_values = JSON.parse(this.responseText);
-
       var output = '';
-      for (var i in json_values) {
-        if (search_term == json_values.name[i])
+      // for (var i in json_values) {
+      for (var i = 0; i < json_values.length; i++) {
+        if (search_term == json_values[i].name)
         output = '<div class="card">' +
           '<div class="card-body">' +
             '<h5 class="card-title">' + json_values[i].name + '</h5>' +
             '<p class="card-text">' + json_values[i].description + '</p>' +
           '</div>' +
-        '</div> something';
-      // }
+        '</div>';
+      }
       document.getElementById('description').innerHTML = output;
     }
   }
   xhr.send();
 }
+
+// const desc = [
+//   {
+//     "namex": "Containers",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Scrollspy",
+//     "description": "Scrollspy has a few requirements to function properly:<br><ul><li>If you’re building our JavaScript from source, it requires util.js.</li><li>It must be used on a Bootstrap nav component or list group.</li><li>Scrollspy requires position: relative; on the element you’re spying on, usually the <body>.</li><li>When spying on elements other than the <body>, be sure to have a height set and overflow-y: scroll; applied.</li><li>Anchors (<a>) are required and must point to an element with that id.</li></ul><p>When successfully implemented, your nav or list group will update accordingly, moving the .active class from one item to the next based on their associated targets.</p>",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Tooltips",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Toasts",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Spinners",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Progress",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Popovers",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Pagination",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Navbar",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Navs",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Modal",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Media object",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "List group",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Forms",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Dropdowns",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Collapse",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   },
+//   {
+//     "namex": "Carousel",
+//     "description": "Containers are the most basic layout element in Bootstrap and are required when using our default grid system. Choose from a responsive, fixed-width container (meaning its max-width changes at each breakpoint) or fluid-width (meaning it’s 100% wide all the time).<br><br>While containers can be nested, most layouts do not require a nested container.",
+//     "output": "xyz"
+//   }
+// ];
+
+
+// var getdescription = function (search_term) {
+//   loady();
+// }
+
+// var loady = function(optn) {
+//   document.getElementById('description').innerHTML = `
+//   ${desc.map(gd).join('')}
+//   `
+// }
+
+// var gd = function(a) {
+//   return `
+
+//   <div class="card">
+//     <div class="card-body">
+//       <h5 class="card-title"> ${a.namex}></h5>      
+//       <p class="card-text"> ${a.description} </p>
+//     </div>
+//   </div>
+//   `
+// }
+
 
 
 /////////////////////////////////////////////////////////////////////
@@ -49,8 +163,11 @@ var searchx = function () {
   var output;
   var content = document.getElementById('content');
   var description = document.getElementById('description');
+  // if (search_term == '') {
+
+  // }
   if (found) {
-    content.style = 'display: block;'; console.log('reached');
+    content.style = 'display: block;'; // console.log('reached');
     description.style = 'display: none;';
     content.innerHTML = "<button type=\"button\" class=\"list-group-item list-group-item-action op\" onclick=\"getdescription(\'" + options[position] + "\');\">" + options[position] + "</button>";
   }
